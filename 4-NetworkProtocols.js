a63b684816edbf5ed59bc17f43ba6cd8b326013a
ivanfslee <ivanfslee@gmail.com>
2020-02-24 12:01:10 -0800

    Added notes to Network Protocols - Systems Expert

--- 4-NetworkProtocols.js
+++ 4-NetworkProtocols.js
@@ -9,9 +9,36 @@
             //Allows for ordered, reliable data delivery between machines over
             //public internet by creating a 'connection'
 
-            //TCP typically implemented in the kernal, which exposes 'sockets' 
+            //TCP typically implemented in thekernal, which exposes 'sockets' 
             //to applications that they can use to stream data through an open connection
 
         //3. HTTP - 'HyperText Transfer Protocol'
-            //
-        //4. IP Packet
+            //Common network protocol built on top of TCP.
+            //Clients make an HTTP request to a server and server responds
+            //HTTP requests typically have a schema:
+                //host: string (e.g. algoexpert.io)
+                //port: integer (e.g. 80 or 443)
+                //method: string (e.g. get, put, post, delete, options, patch)
+                //headers: pair list (e.g. 'Content-type' => 'application/json)
+                //body: opaque sequence of bytes
+
+            //Example Response schema
+                //status code: integer (e.g. 200, 401)
+                //headers: pair list (e.g. 'content-length' => 1238)
+                //body: opaque sequence of bytes
+        //4. IP Packet -
+            //sometimes called 'packet' or network packet. 
+            //IP packet is the smallest unit used to describe data being sent over IP, aside from bytes
+            //IP packet consists of:
+                //IP header - contains source and destination IP addresses 
+                    //as well as other info related to network
+                //payload
+                    //data being sent over the network
+
+    //Typically Network Protocols are low-level and swe's will not deal with them on a daily basis
+    //In the context of Systems Design Interviews, understanding them at a high level is what is needed
+    
+    //What is a protocol?
+        //An agreed upon group of rules and procedures for an interaction between 2 parties
+        //e.g. Protocol for human interaction when you bump into them in the hallway
+        //2:42
