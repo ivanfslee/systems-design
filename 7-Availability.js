9592af055281a5e8a1c8b1c44b66a51d00b5176c
ivanfslee <ivanfslee@gmail.com>
2020-02-27 08:46:04 -0800

    Added notes on Systems Expert video 7 - Availability

---
+++ 7-Availability.js
@@ -0,0 +1,49 @@
+//Key Terms
+    //Process
+        //A program currently running on a machine/
+        //In a sufficiently large system, you should assume that a process
+        //may get terminated at any time 
+
+    //Server
+        //A machine or process that provides data/service for a client.
+        //Typically by listening for incoming network calls/requests.
+
+        //A single machine or piece of software can be both a client and a server at the same time
+        //E.g. single machine can be a server for end users and as a client for a database
+
+    //Node/Instance/Host
+        //These three terms refer to the same thing most of the time. 
+            //A virtual or physical machine on which the developer runs processes
+            //Sometimes, the word 'server' also refers to this concept as well.
+
+    //Availability
+        //The likelihood/odds of a particular server or service being up and running at any point in time. 
+        //Typically measured in percentages.
+        //A server that has 99% availability will be operational 99% of the time
+        //This would be said to have 'two nines' of availability
+
+    //High Availability
+        //Used to describe systems/service that have high level of availability - usually '5 nines' or more. (99.999%)
+        //Sometimes abbreviated 'HA'
+
+    //Nines
+        //Refers to percentages of uptime 
+            //99% - two nines - 97 hours of downtime per year
+            //99.9% - three nines - 8.7 hours of downtime per year
+            //99.99% - 4 nines - 52 minutes of downtime a year
+            //99.999% - 5 nines - 5 minutes of downtime per year 
+
+    //Redundancy
+        //Process of replicating parts of a system in order to reduce downtime and increase reliability
+
+    //SLA
+        //'Service-level agreement'
+        //SLA is a collection of guarantees given to a customer by a service provider
+        //SLAs usually make guarantees on a system's availability, amongst other things
+        //SLAs are made up of one or multiple SLOs (service-level objectives)
+
+    //SLO
+        //'service-level objective'
+        //A guarantee given to a customer by a service provider. 
+        //SLOs usually make guarantees on a systems availability, amongst other things 
+        //SLAs are made up of SLOs
