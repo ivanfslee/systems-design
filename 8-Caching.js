63caada42155f5c08ac79be9dfaed608bce6b6fd
ivanfslee <ivanfslee@gmail.com>
2020-02-27 22:48:47 -0800

    Added notes to Systems Expert video 8 Caching

---
+++ 8-Caching.js
@@ -0,0 +1,19 @@
+//Key Terms
+    //Cache
+        //A piece of hardware or software that stores data
+        //Typically meant to retrieve that data faster than otherwise
+
+        //Caches often used to store responses to network requests as well as 
+        //results of computationally-long/intensive operations
+
+        //Note that the data in a cache can become 'stale' if the main source 
+        //of truth for that data (e.g. the main database behind the cache)
+        //gets updated and the cache doesnt
+        
+    //Cache Hit
+
+    //Cache Miss
+
+    //Cache Eviction Policy
+
+    //Content Delivery Network
