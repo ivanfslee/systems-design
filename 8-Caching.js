c10d1fb9dd90a9736ec8ad2648f5039eb5673ff3
ivanfslee <ivanfslee@gmail.com>
2020-02-28 10:18:34 -0800

    Added notes on Systems Expert video 8 Caching

--- 8-Caching.js
+++ 8-Caching.js
@@ -11,9 +11,28 @@
         //gets updated and the cache doesnt
         
     //Cache Hit
+        //When requested data is found in a cache
 
     //Cache Miss
+        //When requested data could've been found in a cache but isn't.
+        //Typically used to refer to a negative consequence of a system failure or a poor design choice. 
+
+        //e.g. If a server goes down, our load balancer will have to forward requests to a new server
+            //which will result in cache misses
 
     //Cache Eviction Policy
+        //The policy/criterira by which values get evicted or removed from a cache. 
+
+        //Common cache eviction policies include:
+            //LRU - least-recently used
+            //FIFO - first-in-first-out
+            //LFU - least-frequently used
 
     //Content Delivery Network
+        //Third-party service that acts like a cache for your servers
+        //Sometimes web apps can be slow for users in a certain region if the servers are located in a another region
+
+        //A CDN has servers all around the world
+        //Which means latency to a CDN's servers will more likely be far better than latency to your servers 
+
+        //Two most popular CDNs are Cloudflare and Google Cloud CDN
