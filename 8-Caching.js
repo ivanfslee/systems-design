a16fcaf8a7beb4357f2b109c3a102de33164d416
ivanfslee <ivanfslee@gmail.com>
2020-03-01 14:57:07 -0800

    Added notes from Systems Expert video 8 Caching

--- 8-Caching.js
+++ 8-Caching.js
@@ -78,7 +78,7 @@
         //Concrete instances of caching
             //You are doing a lot of network requests, but you want to avoid doing a bunch of network requests
 
-            //Situation:
+            //Situation 1: Avoiding making the network request multiple times
                 //Client makes request to -> server 
                 //Server makes request to -> database
 
@@ -89,6 +89,43 @@
                 //In order to avoid doing that network request again to the database, it can be cached
                 //Accomplished by having the client or server cache that value from the database request
 
-            //Another situation:
-            //Caching to avoid redoing a computationally long/intensive operation
+            //Another situation 2: Avoiding the request to redo a computational long operation
+                //Caching to avoid redoing a computationally long/intensive operation
+
+                //Maybe the client requests something from the server
+                //The server then has to do some very long/intensive operation (Very long time complexity)
+                //You may want to cache that. 
+            
+            //Another situation 3: Avoid doing the same operation no necessarily due to speed, but it may slow down your system in other ways
+            //Most of the time, caching is used to speed up the system
+                //Though not always the case. 
+                //Imagine you have multiple servers
+                //And all the servers are hitting the database with the same request 
+                //Perhaps you have a bunch of clients doing the same thing 
+                //All the clients are making the same request to the same Instagram profile 
+                
+                //Here you can use caching, not to increase the speed of the network request necessarily
+                //But instead, you don't want to read the database 1000 times for the same thing 
+                //because that may overload the database
+                //So you would use caching to not have to read from the database that many times 
+
+//Algoexpert Example
+    //1 - Caching the questions list
+    //First time you go to algoexpert, 
+
+    //Caching the questions list on the client 
+
+    //The list of questions is static content
+        //So instead of having the client make the request to the server to get the questions list everytime,
+        //We cache the questions list on the client after the first time client accesses the questions list. 
+    
+    //2 - Running code on algoexpert 
+        //Running user code on algoexpert takes around 1 second. 
+
+        //To run the algoexpert solution code - it takes very little time, because those values from the solution are cached
+        //Thus, the algoexpert solution will be in the order of milliseconds rather than
+        //a full second like running user submitted code 
+
+
+
             
