2fef5a1d706624a28ad0e3697b490644dda59aef
ivanfslee <ivanfslee@gmail.com>
2020-03-02 11:08:19 -0800

    Added rest of notes for Systems Expert video 9 Proxies

--- 9-Proxies.js
+++ 9-Proxies.js
@@ -57,5 +57,45 @@
 
         //The client will not know that the request went to a reverse proxy
         //The client will just think that the request went to its intended destination server
-        //705
+        //Then the reverse proxy will forward the request to the server
+
+        //Client sends response to server
+            //Client -> Reverse Proxy -> Server 
+        
+        //Server sends back response to client 
+            //Server -> Reverse Proxy -> Client 
+
+        //They main point is that the client thinks it is interacting directly with the server
+        //To the client, there is only one entity instead in actuality, it is two (reverse proxy and server)
+
+        //The situation is just the reverse of the Forward proxy situation
+            //Server receives a request from the Forward Proxy 
+            //Server sends back a response to the Forward Proxy
+            //Server doesn't know that the Forward Proxy is a different entity from the client 
+        
+        //Reverse Proxy are really useful because they can do alot
+            //Can use reverse proxies to filter out certain requests
+            //For example, you may not want Server to deal with a certain kind of request 
+            //The reverse proxy will filter those requests 
+
+            //Reverse proxy can log stuff, analytics
+            //Reverse proxy can cache things like HTML pages, to take load off of the server 
+
+        //One of the most useful things a Reverse Proxy can do:
+            //Use reverse proxy as a load balancer 
+            //Load balancer - something that can distribute load, request loads, amongst a bunch of servers 
+            //You may have multiple servers 
+            //You can have the reverse proxy server as a load balancer to decide which server 
+            //requests will go to
+
+            //Security usage- 
+                //A malicious client may issue a ton of requests 
+                //Reverse proxy can distribute those requests amongst a bunch of servers 
+                //So that no single server gets taken down 
+            
+        //NgineX - a webserver
+            //Can be used as a reverse proxy 
+            
+
+
         
