7c96e4fb274c7fb23826d6f89a7975af041164aa
ivanfslee <ivanfslee@gmail.com>
2020-03-01 16:15:07 -0800

    Added more notes to Systems Expert video 8 Caching

--- 8-Caching.js
+++ 8-Caching.js
@@ -126,6 +126,59 @@
         //Thus, the algoexpert solution will be in the order of milliseconds rather than
         //a full second like running user submitted code 
 
+        //How do you cache the running code?
+            //You could store the cache in a separate component from the server like 'redis' - an in-memory database (key-value store)
 
+            //Client writes their solution, sends it the algoexpert server to check if it is right
+            //Server gets the request. Which is a bunch of bytes. We hash the request to a single signature
+            //The signature is a single integer
+            //We then compare the integer/signature to our cache stored in redis or server
+            //We check if that signature matches the key in the cached key-value store 
+            // We then return the cached value to the client 
+            //This avoids doing the additional 1 second computation
+            //We effectively just return the value we had in our cache to save us from doing that computation of running the users code 
 
+    //Writing to server/database
+        //Client writes a post (e.g. facebook/linkedin/etc) sends it to server 
+        //Post is then stored in database
+
+        //Caching the post at the server level in memory 
+            //Now, your post exists in two places. (2 sources of truth)
+                //1. cached in the server
+                //2. stored in the database
+        
+        //Let's say you edit the post 
+            //Client edits the post 
+            //Server makes a request to the database
+            //How to deal with these 2 sources of truth?
+
+                //1. write-through cache
+                    //When you write a piece of data that exists in 2 places e.g. is cached in one place and stored in the database
+                    //Your system will write that piece of data both in the cache and in the database in the same operation
+                    //This way, the cache and the database are always in-sync 
+
+                    //So if you are editing your post
+                        //Client makes request to the server
+                        //Edits the cache in the server
+                        //Also edits the post in the database
+                    
+                    //The downside to this is that you still have to go to the database
+                
+                //2. write-back cache
+                    //Youre a user, you edit your post
+                    //That makes a network request to the server
+                    // Server will update the cache, but ONLY the cache
+                    // So that means, your cache will be out of sync with your database 
+                    // Your systems will ASYNCHRONOUSLY update your database with the values stored in the cache 
+                        //This will be done in a variety of ways
+                            //Could update the database entry every 5 seconds/ 5 minutes /5 hours etc 
+                            //Could evict stuff out of the cache when cache gets filled up THEN the database is updated 
+
+                    //The downside to this is if something happens to your cache BEFORE you update the database
+                        //You will lose data. Though, there are some ways to mitigate that.
             
+    //Design Youtube Comment Section System
+    
+    
+                    
+                    
