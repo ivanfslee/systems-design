659629569fe2457d3ac537dfad00604ba9e73d34
ivanfslee <ivanfslee@gmail.com>
2020-03-03 15:03:43 -0800

    Added more notes to Systems Design video 10 Load Balancers

--- 10-LoadBalancers.js
+++ 10-LoadBalancers.js
@@ -25,3 +25,49 @@
     //Nginx
         //Specific piece of technology/product 
         //Popular web server that is often used as a reverse proxy and load balancer
+
+//Load Balancers
+    //Most likely will use load balancer in all your systems design interviews
+
+    //Multiple clients
+    //Client -> Load Balancer -> Server
+    //Client
+    //Client 
+
+    //Some clients might be issuing multiple requests to the server 
+
+    //In this example, we won't worry about the server's responses back to the client
+
+    //Our single server has limited resources and can only handle a limited amount of requests in a certain time period
+    //Our system has limited throughput 
+
+    //The more requests that are sent from one client or multiple clients to our server
+    //The more likely it is that our server becomes overloaded with more requests than it could handle
+    //Which may lead to a failure of the system or the system becomes really slow
+    
+    //How to resolve this?
+        //We scale the system
+            //Vertically - increase the power of our server (this will be limited)
+
+            //Horizontally - add more servers 
+        
+    //Let's say we add more 3 more servers 
+    //The system can then process roughly 4 times the amount of requests
+    //But only if the requests are distributed equally amongst the servers 
+
+    //How to ensure this balanced distribution from the clients to the servers?
+        //Usage of a load balancer 
+
+    //Load Balancer - server that sits between clients and servers 
+        //Job is to balance workload across resources 
+        //In this example, job is rerouting traffic in the form of client requests to the servers in a balanced/preconfiguired way
+
+    //Client
+    //5:30
+
+
+
+
+
+
+
