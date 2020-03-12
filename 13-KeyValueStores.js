83dfb58641baaa7cd60a6715a58fd69aede7bd71
ivanfslee <ivanfslee@gmail.com>
2020-03-12 15:15:26 -0700

    Added notes for Systems Expert video 13 KeyValueStore

---
+++ 13-KeyValueStores.js
@@ -0,0 +1,16 @@
+//Key Words
+    //Relational Database
+
+    //Non-relational Database
+
+    //Key-Value Store - A flexible NoSQL database thats often used for caching and dynamic configuration.
+        //Popular options include DynamoDB, Etcd, Redis, ZooKeeper
+
+        //Etcd 
+
+        //Redis
+
+        //ZooKeeper
+
+
+
