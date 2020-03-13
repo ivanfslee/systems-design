b6e6d28a88cce930fea7a61eeb6c5a8fcf168a47
ivanfslee <ivanfslee@gmail.com>
2020-03-13 09:42:36 -0700

    Added notes from Systems Design video 13 Key Value Stores

--- 13-KeyValueStores.js
+++ 13-KeyValueStores.js
@@ -6,11 +6,22 @@
     //Key-Value Store - A flexible NoSQL database thats often used for caching and dynamic configuration.
         //Popular options include DynamoDB, Etcd, Redis, ZooKeeper
 
-        //Etcd 
+        //Etcd - A strongly consistent and highly available key-value store that's often used to implement
+            //leader election in a system
+            //https://etcd.io/
 
-        //Redis
+        //Redis - An in-memory key-value store. 
+            //Does offer some persistent storage options but is typically used as a
+            //really fast, best-effort caching solution 
+            //Redis is also often used to implement rate limiting
+            //https://redis.io/
 
         //ZooKeeper
+            //A strongly consistent, highly available key-value store.
+            //Often used to store important configuration 
+            //or to perform leader election.
+            //https://zookeeper.apache.org/
+    
 
 
 
