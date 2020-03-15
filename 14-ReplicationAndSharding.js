ff8d07aa9857e1130d02d66a9b5362d0c9de8b9c
ivanfslee <ivanfslee@gmail.com>
2020-03-15 16:06:21 -0700

    Added notes from Systems Design video 14 Replication and Sharding

--- 14-ReplicationAndSharding.js
+++ 14-ReplicationAndSharding.js
@@ -37,7 +37,8 @@
 //Main Database -> Sync  -> Replica
 //Main Database -> Async -> Replica
 
-    //Situation: 
+    //Situation: Using replication to increase availability
+
         //Database goes down -> Subsequently, system goes down
         //How to prevent this?
         //Replicate the database. Think of the replica as in standby
@@ -55,29 +56,51 @@
             //Main Database -> Sync  -> Replica
             //If you want your replica to be able to takeover in the event your main db goes down
             //Whenever there is a write operation to the main db -> You need to write to replica as well
-            
 
+            //If for whatever reason the write operation to the replica fails, the write operation to the main database should not be done 
+            //You never want your replica out of sync with your main db
+            //This also means that your writing to the db will take a little longer, because you have to write to the replica as well
 
+            //As a result, with replication, you are increasing the availability of your db and subsequently, your system as a whole
 
+            //We can also use replication to increase our db's latency and as a result our systems latency
 
+    //Situation: Using replication to improve latency
+        //We are designing LinkedIn
+        //People write posts and other people view those posts
+        //We have users in US and India
+        //We will have 2 db, one in USA, serving USA users
+        //One db in India, serving India users
 
+        //Users in USA and India will experience lower latency because they are being 
+        //served by db in their respective regions
+    
+        //I am a US user
+            //I write a post
+            //The post will be written to US db
+            //My post needs to be replicated in the India db
+            //I may have friends in my LinkedIn account in India
+            //So I need my post to appear in the Indian friends linkedIn feeds
 
+            //However, maybe I don't need my post to show up right away in india users
+            //In that case, I can ASYNCHRONOUSLY write to the replica db (india db)
+            //Every 10 min/5 min/some time period, the main db will async with the replica db (india db)
+            //And the india db will do the same with the usa db
 
+            //The two databases (US and India) will update each other
 
+            //This is not necessarily how linkedIn is designed, but it is a way you COULD design it
 
+            //This is an example you could use replication to lower latency
+            //If you don't mind async updating db 
+            //That is, in the cases that you don't need to synchronize your data right away at the moment the data is being written to the db
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            //Another example, you are deploying piece of software to production
+            //You have to serve multiple regions across the world
+            //You may want to update your regional db first
+            //Then update the other overseas db asynchronously. 
+            //You roll out the update regionally.
+    
+    //Situation:
+        //You have one main db
+        
