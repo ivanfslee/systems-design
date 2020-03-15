6d3aeb2230139fa01f41fbdcba05ead45146d1df
ivanfslee <ivanfslee@gmail.com>
2020-03-15 14:21:53 -0700

    Added notes from Systems Design video 14 Replication and Sharding

--- 14-ReplicationAndSharding.js
+++ 14-ReplicationAndSharding.js
@@ -24,3 +24,60 @@
         //or if your workload is naturally skewed
         //Some servers for example will receive a lot more traffic
         //than others, thus creating a 'hot spot'
+
+//Data redundancy and data partitioning techniques can be used to enhance 
+//a system's fault tolerance, throughput, and overall reliability
+
+//A systems performance is often dependent on its database's performance
+    //If a systems database is not available, the system itself will likely be unavailable
+    //If a systems database, latency is high and throughput is low , then the system itself will be the same
+
+    //When we design a system that is performant, we have to make sure that the database is performant as well
+        
+//Main Database -> Sync  -> Replica
+//Main Database -> Async -> Replica
+
+    //Situation: 
+        //Database goes down -> Subsequently, system goes down
+        //How to prevent this?
+        //Replicate the database. Think of the replica as in standby
+
+        //The main database has reads/writes performed on it 
+        //But it also submits those changes to the replica
+
+        //The replica would take over if the main database fails 
+        //Once the main db comes back up again, it would be updated by the replica 
+
+        //Then main db becomes the main db again and replica goes back to being a replica
+
+        //Your replica always needs to be up to date with the main db
+        //So whenever main db is updated, the replica has to be updated as well - in a SYNCHRONOUS way 
+            //Main Database -> Sync  -> Replica
+            //If you want your replica to be able to takeover in the event your main db goes down
+            //Whenever there is a write operation to the main db -> You need to write to replica as well
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
