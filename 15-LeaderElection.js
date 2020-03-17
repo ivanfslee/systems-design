f674c74e282d0f86ec0850c58e91e9c5f184d3de
ivanfslee <ivanfslee@gmail.com>
2020-03-16 23:38:30 -0700

    Added notes from Systems Expert, video 15 Leader Election

--- 15-LeaderElection.js
+++ 15-LeaderElection.js
@@ -24,3 +24,35 @@
         //A strongly consistent, highly available key-value store. 
         //Often used to store important configuration or to perform leader election.
         // https://zookeeper.apache.org/
+
+//Leader Election is typically an advanced aspect in systems design
+//In the context of a systems design interview, only a high-level understanding is needed
+
+//A system that you might want leader election
+    //Netflix/Amazon Prime - a service where users want to subscribe on an annual basis
+
+    //Third-Party Service        Database
+
+    //Database contains the users and the status of their subscription
+        //Also contains the date their subscription is supposed to renew
+        //Also contains the price the user will be charged on a recurring basis
+
+    //You might be using some third-party service 
+        //Actually charges the user
+        //The debit transactions from the user's bank accounts
+        //e.g. paypal, stripe 
+        //So this means your third party service needs to communicate with your database
+        
+        //You may not want the 3rd party service to interact with your database directly
+            //Also, that may not be possible because 3rd party can't connect directly to db
+            //Even if it were possible, you may not want to do that
+                //Your db is a sensitive part of your system
+                //Because of that, you may not want a 3rd party service to connect directly to it
+                //A reasonable thing to do would be to have some kind of service in the middle
+                //in between the 3rd party and the database
+
+                //3rd party service -> middle service  <- Database
+
+                //That middle service is in charge of communicating with the db
+                //figure out when a users subscription is up for renewal
+
