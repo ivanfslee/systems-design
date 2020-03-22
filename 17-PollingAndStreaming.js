15c48f31dceaca430788c185aa4038d4c9c9e5e0
ivanfslee <ivanfslee@gmail.com>
2020-03-22 16:58:35 -0700

    Added notes from Systems Expert video 17 Polling and Streaming

--- 17-PollingAndStreaming.js
+++ 17-PollingAndStreaming.js
@@ -13,4 +13,28 @@
         //getting a feed of information from a server by keeping an 
         //open connection between the two machines or processes.
     
+//So far, we have dealt with systems where clients have issued requests to servers
+//And servers send response back to the client
+
+//What happens if our system has clients that want a piece of data
+//that gets updated/changes regularly
+    //e.g. a system that monitors the temperature outside 
+    //The temperature changes alot
+    //Your servers need to know the temperature
+    //Your clients need to be served the temperature
+
+    //e.g. a chat app
+    //You have people who are meant to be able to communicate with each other
+    //in real-time
+    //You need to be able to send and receive messages in real-time
+
+//In both these examples, you are dealing with data 
+//that gets updated regularly
+//and clients need to be able to see regularly
+
+//How to build a system to reflect these updates?
+    //2 techniques to achieve this
+        //Polling
+        //Streaming
     
+    //Polling - 
