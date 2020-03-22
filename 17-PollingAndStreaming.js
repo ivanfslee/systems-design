5ac620533c180639e3e925187f5a69c523ab26c8
ivanfslee <ivanfslee@gmail.com>
2020-03-22 15:16:27 -0700

    Added notes from Systems Expert video 17 Polling and Streaming

---
+++ 17-PollingAndStreaming.js
@@ -0,0 +1,16 @@
+//Key Terms
+    //Socket
+        //A kind of file that acts like a stream.
+        //Processes can read and write to sockets and communicate in this manner. 
+        //Most of the time, the sockets are fronts for TCP connection.
+    
+    //Polling
+        //The act of fetching a resource or piece of data regularly at an interval
+        //to make sure your data is not too stale.
+    
+    //Streaming
+        //In networking, it usually refers to the act of continuously 
+        //getting a feed of information from a server by keeping an 
+        //open connection between the two machines or processes.
+    
+    
