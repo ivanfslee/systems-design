59d587e06a060c553bd01301456dbb070835e0c4
ivanfslee <ivanfslee@gmail.com>
2020-03-23 14:43:43 -0700

    Added notes from Systems Expert video 18 Configuration

--- 18-Configuration.js
+++ 18-Configuration.js
@@ -1,8 +1,27 @@
 //Key Term
     //JSON
+        //A file format heavily used in APIs and configuration. 
+        //Stands for 'JavaScript Object Notation'
+
+        //e.g. 
+        //{
+            //'version': 1.0,
+            //'name': 'AlgoExpert Configuration'
+        //}
 
     //YAML
+        //A file format mostly used in configuration. 
+        //Example:
+            //version: 1.0
+            //name: AlgoExpert Configuration
 
     //Key-Value Store
+        //A flexible NoSQL database that is often used for caching and dynamic configuration.
+        //Popular options include DynamoDB, Etcd, Redis, and ZooKeeper
 
     //Configuration
+        //A set of parameters or constants that are critical to a system.
+        //Configuration is typically written in JSON or YAML
+        //And can be 'static' - meaning it is hard-coded in and shipped with your systems application code (like frontend code for example)
+        //Or it can be 'dynamic' - meanint that it lives outside of your system's application code.
+
