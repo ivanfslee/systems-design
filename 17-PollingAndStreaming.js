99e1ba2d563c4abff3fb43ead622f6b5878d3b18
ivanfslee <ivanfslee@gmail.com>
2020-03-23 00:02:58 -0700

    Added notes from Systems Expert video 17 Polling and Streaming

--- 17-PollingAndStreaming.js
+++ 17-PollingAndStreaming.js
@@ -65,4 +65,72 @@
             //be millions of requests per second
             //which is alot of load for your servers
             
-    //Streaming - instead of having your client repeatedly 
+    //Streaming - instead of having your client repeatedly request data from server
+        //Client will open a long-lived connection with your server
+        //This is done through a socket - 
+
+        //socket - a file that lives on your computer, that your comp can write to and read from
+            //to communicate with another computer
+            //in a long-lived connection capacity
+
+            //You can think of a socket as a portal into another computer
+            //That you can communicate through
+
+            //Without having to repeatedly send requests
+
+            //Just an open connection between 2 machines that exist as long as neither
+            //machine closes it or so long as the network is healthy
+
+            //Client opens a long-lived connection with a socket
+            //and listens to your server for any data that your server might push to it
+
+            //Server will push data (in the case of chat application)
+                //Server pushes the messages through the connection to your client
+                //and that will be the streaming in action
+            
+            //So your client will be streaming data from the server
+
+            //The key point is unlike with polling where the client is the one requesting data
+                //and the server is only sending data only when it receives request from the client
+                //In streaming, your client is just listening to the server
+                    //It is the servers job to send the data out to the client
+                    //The server is no longer passively listening to requests
+                    //The server will proactively send data to the client
+
+                    //'Pushing' - refers to server sending data to client
+                        //Server, instead of passively listening to requests from client
+                        //Server will proactively push data to the client
+
+             //Client <<----- Server
+
+             //Streaming allows for a continous stream of data
+
+            //In the case of the chat app, streaming allows:
+                //allows clients to continuously receive messages (without repeatedly request them from server)
+                //so long as server correctly pushes messages to client
+            
+                //You can attain the instantaneous aspect of your chat app
+                    //without having alot of requests
+                    //just one long-lived connection per client
+            
+    //Important note:
+        //Streaming is not necessarily better than polling
+
+        //For the chat app - streaming is better than polling
+
+        //Depending on your use-case
+            //Polling might be better than streaming
+            //Or streaming might be better than polling
+        
+        //Your job in a systems design interview, you need to figure out
+            //out what functionality your system will support 
+            //and to then decide how best to implement those functions (polling, streaming, or neither)
+
+            //General rule of thumb - if you need some kind of instantaneous experience
+                //or you need data updated very frequently
+                //You are likely going to use streaming
+            
+                //If you need data updated not too frequently
+                    //e.g. every 20 seconds
+                    //Polling might be useful
+        //11:24
