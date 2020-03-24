91603a722ac912bf76d145c0cc8b9ec0a93f51a4
ivanfslee <ivanfslee@gmail.com>
2020-03-24 00:26:40 -0700

    Added notes from Systems Expert video 18 Configuration

--- 18-Configuration.js
+++ 18-Configuration.js
@@ -25,3 +25,69 @@
         //And can be 'static' - meaning it is hard-coded in and shipped with your systems application code (like frontend code for example)
         //Or it can be 'dynamic' - meanint that it lives outside of your system's application code.
 
+//A simple part of systems design but often overlooked
+//Most large scale distributed systems will rely on configurations
+
+//What is config
+    //A set of parameters/ constants that your system or application will use
+    //But instead of having these constants being embedded in your code
+    //You will write/configure them in a seemingly isolated file
+
+    //Typically config is written in JSON or YAML
+        //This makes it easy to edit regardless of the language of your underlying application
+    
+    //Typically config files are named - 'config.json' / 'config.yaml'
+
+
+    //Very simply, you can think of config as the set of constants you use in your application
+        //This is why config is often overlooked.
+        //As it seems so simple at face value.
+
+    //JSON vs YAML 
+        //Depends on the team/company and what you prefer
+        //Uber used YAML
+        //Google - used either, depending on the team/product you are on
+        //Algoexpert uses JSON
+
+    //Sample config file JSON:
+
+// {
+//     "apiKey": "absdfsdf_dlfjd",  (use config to store apiKeys)
+//     "showSystemsExpert": false,  (use config to show if new feature is visible or not)
+//     "supportedLanguages": [      (define programming languages they support on the platform)
+//         "cpp",
+//         "csharp",
+//         "go",
+//         "java",
+//         "javascript",
+//         "python",
+//         "swift"
+//     ],
+//     "version": {                 (define version of their application)
+//         "number": 0,
+//         "releaseDate": "2020-02-20"
+//     }
+// }
+
+
+    //Sample config file YAML:
+
+// apiKey: aldfjlsdfj
+// showSystemsExpert: false
+// supportedLanguages:
+// - cpp
+// - csharp
+// - go
+// - java
+// - javascript
+// - python
+// -swift
+// version:
+//  number: 0
+//  releaseDate: '2020-02-20'
+
+
+//Two primary types of configuration
+    //1. Static configuration
+
+    //2. Dynamic configuration
