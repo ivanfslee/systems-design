7d186c21d70fb76311bed82c3fbd8b4b729aa297
ivanfslee <ivanfslee@gmail.com>
2020-03-28 17:13:29 -0700

    Added notes from Systems Expert video 20 Logging and Monitoring

--- 20-LoggingAndMonitoring.js
+++ 20-LoggingAndMonitoring.js
@@ -54,3 +54,44 @@
             //that will collect all your logs and store them in some kind of database
             //so you can go to that database
 
+            //You look through the logs at a later time in order to debug that issue
+        
+        //In practice, when you are developing your app
+            //You will put into the code, things like:
+                //errors
+                //important operations
+                //requests your users are doing
+
+            //2 popular formats of logs:
+                //syslog
+                //JSON
+
+            //You will have a service that collects these logs
+                //and stores them in some database
+            
+            //Algoexpert uses google stack driver
+                //to store their logs
+            
+            //Then when user comes to you with a problem
+                //you look through your logs around the time they purchased your product
+                //to try to debug the users problem
+                //by looking through their actions 
+                //and see what may have gone wrong
+            
+            //Logging is simple but is what allows you to debug issues
+                //when you are dealing with users/problems at scale
+            
+            //Typically if you are working at a large company
+                //or a company that has rigorous engineering practices
+                //in place
+                //you will have to have logs in your system
+    
+    //Monitoring
+        //tool or thing that makes managing a system alot easier
+        //and alot better
+
+        //As your system grows, you need 
+            //visibility into your systems health , performance, general status
+
+        //by making sure you design your system to gather meaningful metrics 
+        //and that you have tools to monitor those metrics
